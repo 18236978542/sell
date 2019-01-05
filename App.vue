7f38cce02a36ee602536984e7c9a22a9bd589f0c
18236978542 <46392515+18236978542@users.noreply.github.com>
2019-01-05 14:17:23 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+  	
+  	<vhead></vhead>
+  	
+  	<div class="tab">
+  		
+  		<div class="tab-item">
+  			<router-link></router-link>
+  		</div>
+  		
+  		<div class="tab-item">
+  			<router-link></router-link>
+  		</div>
+  		
+  		<div class="tab-item">
+  			<router-link></router-link>
+  		</div>
+  		
+  	</div>
+  	
+  	<router-view></router-view>
+  	
+  </div>
+</template>
+
+<script>
+	import vhead from './components/head/head';
+	
+	export default {
+	  components: {
+	    vhead
+	  }
+	};
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+</style>
